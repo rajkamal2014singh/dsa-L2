33f46057d4ae3f00aa4ed0e0ca3dd7a27f77bb5b
rajkamal2014singh <rajkamal2014singh@gmail.com>
2026-09-05 21:09:35 +0530

    array and string 4

---
+++ src/ArraysAndString/CarFleet.java
@@ -0,0 +1,65 @@
+package ArraysAndString;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.StreamSupport;
+
+/**
+ * Leetcode car fleet problem
+ * there are n cars and k is the target position
+ * we are provided with the current position of cars in positions array
+ * for every car we are give speed of the car
+ *
+ * we need to find the no of fleets that we will arrive at target after every one reaches the target
+ * single car can also make a fleet
+ * someone from behind cannot overtake the other car in front position, if they reach at same time they form a fleet
+ * or if they are at same position
+ *
+ * example position = [10, 8, 0, 5, 3] speed = [2,4,1,1,3] target = 12
+ * output - 3 (total three fleets will be formed)
+ *
+ *
+ */
+public class CarFleet {
+    public static int countFleets(int[] positions, int[] speed, int target){
+        int n = speed.length;
+        double[][] time = new double[n][2];
+
+        for(int i=0; i<n; i++){
+            double ct = (target * 1d - positions[i] * 1d) / speed[i];
+            time[i][0] = positions[i] * 1d;
+            time[i][1] = ct;
+        }
+
+        Arrays.sort(time, (a, b) -> Double.compare(a[0], b[0]));
+        int count = 0;
+        double tt = 0.0;
+        for(int i=n-1; i>=0; i--){
+            if(time[i][1] > tt){
+                count++;
+                tt = time[i][1];
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] positions = new int[n];
+        int[] speed = new int[n];
+
+        for(int i=0; i<n; i++){
+            positions[i] = scn.nextInt();
+        }
+
+        for(int i=0; i<n; i++){
+            speed[i] = scn.nextInt();
+        }
+
+        int target = scn.nextInt();
+
+        int fleets = countFleets(positions, speed, target);
+        System.out.println("Total fleets formed is " + fleets);
+    }
+}
---
+++ src/ArraysAndString/ValidPalindrome2.java
@@ -0,0 +1,56 @@
+package ArraysAndString;
+
+import java.util.Scanner;
+
+/**
+ *
+ * A string is said to be a valid palindrome when by removing any one character at max makes the string a palindrome
+ * Time constraint - O(n), Space - O(1)
+ *
+ * approach similar as palindrome keep two pointers on start and end
+ * move the pointer till characters are equal
+ * in case of not equal check for the remaining string by removing one character on both the substring
+ * if any one the substring is a palindrome then it is a valid palindrome 2
+ *
+ * Example -
+ * abcefcba -> true -> remove e or f
+ * abcefacba -> false
+ * abceaacba -> true -> remove e
+ *
+ *
+ */
+public class ValidPalindrome2 {
+    public static boolean isPalindrome(String str, int l, int r){
+        while(l < r){
+            if(str.charAt(l) != str.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    public static boolean validPalindrome(String str){
+        int l = 0;
+        int r = str.length() - 1;
+
+        while (l < r){
+            if(str.charAt(l) == str.charAt(r)){
+                l++;
+                r--;
+            }
+            else{
+                return isPalindrome(str, l, r-1) || isPalindrome(str, l+1, r);
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+
+        System.out.println("Can " + str + " become valid palindrome " + validPalindrome(str));
+    }
+}
