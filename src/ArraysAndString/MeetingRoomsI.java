9d71dcc748492139e6c377e7496baec0c0c1b0f2
rajkamal2014singh <rajkamal2014singh@gmail.com>
2026-09-07 01:53:15 +0530

    interval overlapping problems

---
+++ src/ArraysAndString/MeetingRoomsI.java
@@ -0,0 +1,49 @@
+package ArraysAndString;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * Given an n*2 size of array
+ * Tell if a person can attend all the meetings or not
+ *
+ * Approach
+ * Sort the array based on first value
+ * Similar as merge intervals but here if value is overlapping then return false
+ * meaning person cannot attend all the meetings
+ *
+ [ [3 6] [ 1 3] [8 10] [7 8] [9 15] ] -> not possible
+ */
+public class MeetingRoomsI {
+    public static boolean possibleToAttendMeetings(int[][] meetings){
+        Arrays.sort(meetings, ((m1, m2) -> Integer.compare(m1[0], m2[0])));
+
+        for(int i=1; i<meetings.length; i++){
+            if(meetings[i][0] < meetings[i-1][1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][] meetings = new int[n][2];
+
+        for(int i=0; i<meetings.length; i++){
+            meetings[i][0] = scn.nextInt();
+            meetings[i][1] = scn.nextInt();
+        }
+
+        boolean possible = possibleToAttendMeetings(meetings);
+        if(possible){
+            System.out.println("It is possible to attend all the meetings");
+        }
+        else{
+            System.out.println("It is not possible to attend all the meetings");
+        }
+    }
+}
---
+++ src/ArraysAndString/MergeIntervals.java
@@ -0,0 +1,56 @@
+package ArraysAndString;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * Given a n*2 size of array
+ * return a new array where over lapping intervals are merged
+ *
+ * Approach
+ * Sort the array based on first value
+ * Iterate on array and if current elements first value is less than previous elements second value then
+ * merge the elements with max of second value from previous and current
+ [ [3 6] [ 1 3] [8 10] [7 8] [9 15] ]  -> [ [1 3] [3 6] [7 8] [8 15] ]
+ */
+public class MergeIntervals {
+    public static int[][] mergeIntervals(int[][] intervals){
+        Arrays.sort(intervals, ((i1, i2) -> Integer.compare(i1[0], i2[0])));
+        List<int[]> merged = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            if (merged.isEmpty()) {
+                merged.add(interval);
+            } else {
+                int[] prevInterval = merged.getLast();
+                if (prevInterval[1] > interval[0]) {
+                    prevInterval[1] = Math.max(prevInterval[1], interval[1]);
+                } else {
+                    merged.add(interval);
+                }
+            }
+        }
+
+        return merged.toArray(new int [merged.size()][]);
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][] intervals = new int[n][2];
+
+        for(int i=0; i<intervals.length; i++){
+            intervals[i][0] = scn.nextInt();
+            intervals[i][1] = scn.nextInt();
+        }
+
+        int[][] merged = mergeIntervals(intervals);
+
+        System.out.println("Merged interval are ");
+        for(int i=0; i<merged.length; i++){
+            System.out.println(merged[i][0] + " - " + merged[i][1]);
+        }
+    }
+}
